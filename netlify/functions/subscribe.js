c59b95b1a1b6a052d24276b6d3d25810110411e1
asaupe <arne.saupe@gmail.com>
2025-07-11 14:19:16 -0500

    Added some console.log checks to verify API Keys

--- netlify/functions/subscribe.js
+++ netlify/functions/subscribe.js
@@ -1,5 +1,12 @@
 // netlify/functions/subscribe.js
 exports.handler = async (event, context) => {
+    console.log('Environment variables check:', {
+        hasApiKey: !!process.env.KIT_API_KEY,
+        hasFormId: !!process.env.KIT_NEWSLETTER_FORM_ID,
+        apiKeyPrefix: process.env.KIT_API_KEY ? process.env.KIT_API_KEY.substring(0, 10) + '...' : 'undefined',
+        formId: process.env.KIT_NEWSLETTER_FORM_ID
+    });
+
     // Only allow POST requests
     if (event.httpMethod !== 'POST') {
         return {
@@ -20,6 +27,37 @@ exports.handler = async (event, context) => {
             };
         }
 
+        // Check if environment variables are available
+        if (!process.env.KIT_API_KEY) {
+            console.error('KIT_API_KEY is not available');
+            return {
+                statusCode: 500,
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Headers': 'Content-Type'
+                },
+                body: JSON.stringify({ 
+                    success: false, 
+                    error: 'API Key not present' 
+                })
+            };
+        }
+
+        if (!process.env.KIT_NEWSLETTER_FORM_ID) {
+            console.error('KIT_NEWSLETTER_FORM_ID is not available');
+            return {
+                statusCode: 500,
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Headers': 'Content-Type'
+                },
+                body: JSON.stringify({ 
+                    success: false, 
+                    error: 'Form ID not present' 
+                })
+            };
+        }
+
         // Call Kit API
         const response = await fetch(`https://api.kit.com/v3/forms/${process.env.KIT_NEWSLETTER_FORM_ID}/subscribe`, {
             method: 'POST',
@@ -34,6 +72,7 @@ exports.handler = async (event, context) => {
         });
 
         const data = await response.json();
+        console.log('Kit API response:', { status: response.status, data });
 
         if (response.ok) {
             return {
