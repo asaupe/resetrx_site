050fc69e5ff1a7a43f2dcd707054038a8a18aa38
asaupe <arne.saupe@gmail.com>
2025-12-25 15:52:00 -0600

    Add Fixie proxy support for static IP

--- netlify/functions/get-my-ip.js
+++ netlify/functions/get-my-ip.js
@@ -1,8 +1,17 @@
 // Simple function to return the outbound IP address of this Netlify Function
+const { HttpsProxyAgent } = require('https-proxy-agent');
+
 exports.handler = async (event, context) => {
     try {
+        // Setup proxy if configured
+        const fetchOptions = {};
+        if (process.env.FIXIE_URL) {
+            console.log('Using Fixie proxy for IP check');
+            fetchOptions.agent = new HttpsProxyAgent(process.env.FIXIE_URL);
+        }
+        
         // Call a service that returns our IP
-        const response = await fetch('https://api.ipify.org?format=json');
+        const response = await fetch('https://api.ipify.org?format=json', fetchOptions);
         const data = await response.json();
         
         return {
@@ -13,6 +22,7 @@ exports.handler = async (event, context) => {
             body: JSON.stringify({
                 success: true,
                 ip: data.ip,
+                usingProxy: !!process.env.FIXIE_URL,
                 message: 'This is the outbound IP address from this Netlify Function'
             })
         };
